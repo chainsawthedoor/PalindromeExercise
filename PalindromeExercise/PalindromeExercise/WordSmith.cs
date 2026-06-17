f8079365740337860e0f0d073cb72f8de56a9fdc
Jeffrey Gates <jeffreymrgates@gmail.com>
2026-06-17 18:08:52 -0400

    Added WordSmith, created test variables and refactored

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsAPalindrome(string word)
+    {
+        if (word == null || word.Length == 0)
+        {
+            return false;
+        }
+        
+        string wordLower = word.ToLower();
+        string backwards = "";
+
+        for (int i = wordLower.Length - 1; i >= 0; i--)
+        {
+            backwards += word[i];
+        }
+        
+        return wordLower == backwards;
+        
+        // if (word == backwards)
+        // {
+        //     return true;
+        // }
+        // else
+        // {
+        //     return false;
+        // }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,32 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)] // ← you will need to put some test data and expected result here.
+        [InlineData("hannah", true)]
+        [InlineData("kayak", true)]
+        [InlineData("hello", false)]
+        [InlineData("hello world", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+        [InlineData("Racecar", false)]
+        [InlineData("WoWiE", false)]
+        
+        public void Test1(string word, bool expected)
         {
+            //Arrange
+            var smith = new WordSmith();
+            
+            //Act
+            var actual = smith.IsAPalindrome(word);
+            
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
